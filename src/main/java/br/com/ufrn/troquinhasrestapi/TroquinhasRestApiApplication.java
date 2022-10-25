b51f30e11c55a7cede527bdee16faf1a64d5f9f8
Joao de Souza <joaosfvieira@hotmail.com>
2022-10-24 17:31:16 -0700

    first commit

---
+++ src/main/java/br/com/ufrn/troquinhasrestapi/TroquinhasRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufrn.troquinhasrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroquinhasRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroquinhasRestApiApplication.class, args);
+	}
+
+}
